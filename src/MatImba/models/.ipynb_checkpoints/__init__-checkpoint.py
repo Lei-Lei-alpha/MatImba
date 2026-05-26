b5f2690a9c98c7c04b9bbfd5ffc1f6d8f0203268
Lei Lei <Lei.Lei2@nottingham.ac.uk>
2026-05-26 11:34:54 +0100

    Delete src/MatImba/models/.ipynb_checkpoints directory

--- src/MatImba/models/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,9 +0,0 @@
-from .ensemble import gbr_ensemble
-from .fds import FDS
-from .megnet import MEGNet
-from .resnet import ResNet
-
-__all__ = [
-    "gbr_ensemble", "FDS",
-    "MEGNet", "ResNet"
-]
